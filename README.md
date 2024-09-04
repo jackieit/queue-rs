da8c69634507f3dbca7071dde36c608be0b4407e
Jackie <wfits@126.com>
2024-09-04 08:17:42 +0800

    fixed README

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
  Now ,it's only support redis ,may be support other queue later such as db,file and so on.
  ## Usage
  
- 1. how to add a job to queue
+### how to add a job to queue
  
  ```rust
  use queue_rs::queue::Queue;
@@ -32,7 +32,7 @@
  let _job_id = queue.push(TestJob::new("first job".to_string()));
 //!
  ```
- 2. how add a delay job to queue
+### how add a delay job to queue
  ```rust
  let mut queue = Queue::new("queue-test", redis::Client::open("redis://127.0.0.1/").unwrap());
  // will execute after 10 seconds
@@ -40,19 +40,19 @@
  let _job_id = queue.push(TestJob::new("first job".to_string()));
 //!
  ```
- 3. how to listen the queue
+### how to listen the queue
  ```rust
  let queue = Queue::new("queue-test", redis::Client::open("redis://127.0.0.1/").unwrap());
  let task  = QueueTask::new(queue);
  task.listen(0);
  ```
- 4. how to run all jobs in queue, this will exit after all jobs executed
+### how to run all jobs in queue, this will exit after all jobs executed
  ```rust
  let queue = Queue::new("queue-test", redis::Client::open("redis://127.0.0.1/").unwrap());
  let task  = QueueTask::new(queue);
  task.run(0);
  ```
- 5. tracing logs
+### tracing logs
  add tracing-subscriber to cargo.toml
  ```
  tracing-subscriber="0.3"
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! Now ,it's only support redis now,may be support other queue in the future such as db,file and so on
 //! ## Usage
 //!
-//! 1. how to add a job to queue
+//! ### how to add a job to queue
 //!
 //! ```rust
 //! use queue_rs::queue::Queue;
@@ -33,7 +33,7 @@
 //! let _job_id = queue.push(TestJob::new("first job".to_string()));
 //!
 //! ```
-//! 2. how add a delay job to queue
+//! ### how add a delay job to queue
 //! ```rust
 //! let mut queue = Queue::new("queue-test", redis::Client::open("redis://127.0.0.1/").unwrap());
 //! // will execute after 10 seconds
@@ -41,19 +41,19 @@
 //! let _job_id = queue.push(TestJob::new("first job".to_string()));
 //!
 //! ```
-//! 3. how to listen the queue
+//! ### how to listen the queue
 //! ```rust
 //! let queue = Queue::new("queue-test", redis::Client::open("redis://127.0.0.1/").unwrap());
 //! let task  = QueueTask::new(queue);
 //! task.listen(0);
 //! ```
-//! 4. how to run all jobs in queue, this will exit after all jobs executed
+//! ### how to run all jobs in queue, this will exit after all jobs executed
 //! ```rust
 //! let queue = Queue::new("queue-test", redis::Client::open("redis://127.0.0.1/").unwrap());
 //! let task  = QueueTask::new(queue);
 //! task.run(0);
 //! ```
-//! 5. tracing logs
+//! ### tracing logs
 //! add tracing-subscriber to cargo.toml
 //! ```
 //! tracing-subscriber="0.3"
